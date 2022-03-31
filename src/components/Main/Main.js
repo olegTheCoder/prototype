f18073ac275a98b305d56471d1a09e0fad8d11c0
Oleg Grachev <oagrachev@beeline.ru>
2022-03-31 09:34:50 +0300

    from pc

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import styles from './Main.module.scss';
+
+function Main() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.block}></div>
+    </div>
+  );
+}
+
+export default Main;
